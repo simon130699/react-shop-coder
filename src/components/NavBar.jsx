dee3472248fd75fa5b5aac381fe0c122d4518d5f
simon130699 <simon130699@gmail.com>
2021-11-25 01:48:27 -0300

    subida instalando boostrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   <div className="navbar navbar-light pt-0">
   <div className="container-fluid">
     <a className="navbar-brand" href="http://localhost:3000/">
-      <img src="https://export-download.canva.com/Q1wiM/DAEwkcQ1wiM/32/0/0001-12953789289.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20211122%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20211122T234710Z&X-Amz-Expires=72849&X-Amz-Signature=3ae717c0b93dc0f46c22ac690e932aef18feee0140ecf9530248bcc100b354af&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27sp.png&response-expires=Tue%2C%2023%20Nov%202021%2020%3A01%3A19%20GMT" alt="logo" />
+      ReactShop!
     </a>
   </div>
 </div>
